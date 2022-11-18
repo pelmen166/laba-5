079cd388e9e803eae689314de400302f6735c0e8
pelmen166 <92636967+pelmen166@users.noreply.github.com>
2022-11-18 09:13:20 +0200

    Add files via upload

---
+++ 5.0/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лабораторна робота 5.0/Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s = sum(1, 1);
+			Assert::AreEqual(s, 2);
+		}
+	};
+}
